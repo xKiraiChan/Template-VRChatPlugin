78b89cb33ad4257a226ab7873732e79b14ce830c
xKiraiChan <xKiraiChan@gmail.com>
2022-03-26 12:40:19 -0400

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+
+namespace Template_VRChatPlugin
+{
+    [BepInAutoPlugin("com.github.xKiraiChan.Template_VRChatPlugin", "Template_VRChatPlugin", "0.1.0")]
+    public partial class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {Id} is loaded!");
+        }
+    }
+}
